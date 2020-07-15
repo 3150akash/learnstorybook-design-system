469c1208f7413c124f8e756a6275d092433ec433
Akash Rawat <31209881+3150akash@users.noreply.github.com>
2020-07-15 07:38:07 +0000

    added initial commit

---
+++ storybook-static/precache-manifest.272043444519e602db9b111909ef4482.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbd8c05664dd3dfe15e6577f27f3526",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3603e8818fcac6c7817a.bundle.js"
+  },
+  {
+    "url": "runtime~main.3603e8818fcac6c7817a.bundle.js"
+  },
+  {
+    "url": "vendors~main.3603e8818fcac6c7817a.bundle.js"
+  },
+  {
+    "url": "vendors~main.3603e8818fcac6c7817a.bundle.js.LICENSE.txt"
+  }
+]);
